726f09da32e8f0a5fbb7955ac12c324d344f4836
Ajay Kadamm <ajaydkadam99@gmail.com>
2020-02-17 09:59:25 +0530

    lexical changed

--- a6/lexical.cpp
+++ a6/lexical.cpp
@@ -20,20 +20,42 @@ vector<string> OPR = {
 	"<=",
 	"++",
 	"=="};
-int checkTrm(char c)
+	
+	
+vector<string> checkOprAndTrm(vector<string> words){
+string word, opr, trm;
+	for (auto i = words.begin(); i != words.end(); i++)
+		for (auto j = OPR.begin(); j != OPR.end(); j++){
+			word = *i;
+			opr = *j;
+			if(strcmp(word.c_str(),opr.c_str())==0)
+				words.erase(i);
+		}
+	for (auto i = words.begin(); i != words.end(); i++)
+		for (auto k= TRM.begin(); k!=TRM.end(); k++){
+			word = *i;
+			trm = *k;
+			if(strcmp(word.c_str(), trm.c_str())==0)
+				words.erase(i);
+		}
+	return words;
+}	
+vector<string> checkTrm(vector<string> words)
 {
-
-	for (auto i = SYM.begin(); i != SYM.end(); i++)
-	{
-		if (c == (*i))
-			return i - SYM.begin();
-	}
-	return -1;
+	/*char word[30];
+	for(int i=0;i<words.size();i++){
+		word = words[i].c_str();
+		for(int j=0;j<strlen(word);j++){
+			
+		}
+	}*/	
 }
 int main()
 {
-	fstream fp1;
+	fstream fp1,fp2;
 	fp1.open("TRM.txt");
+	vector<string> words;
+	string file1;
 	string line;
 	char str[20];
 	char c;
@@ -47,10 +69,33 @@ int main()
 	// 	cout << TRM[i] << " " << strlen(TRM[i].c_str()) << "\n";
 	fp1.close();
 	fp1.open("input.cpp");
+	fp2.open("temp.cpp", ios::out);
 	while (fp1)
 	{
-		fp1 >> c;
-		cout << c << " " << checkTrm(c) << " trm\n";
+		getline(fp1,line);
+//		cout << " " << line;
+		fp2<<line<<"\n";		
 	}
+	fp1.close();
+	fp2.close();
+	fp2.open("temp.cpp");
+	while (fp2)
+	{
+		fp2 >> line;
+		//cout << line << "\n";
+		words.push_back(line);
+	}
+	words = checkOprAndTrm(words);
+	for(int i=0;i<words.size();i++){	
+		cout<<words[i]<<"\n";
+	}
+	words = checkTrm(words);
+		/*fp2.clear();
+	fp2.seekg(0, ios::beg);
+	while (fp2)
+	{
+		fp2 >> c;
+		cout << c << " " << checkTrm(c) << " trm\n";
+	}*/
 	return 0;
 }
---
+++ a6/temp.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+	int i, j = 1, k = 2, flag;
+	for (i = 0; i <= 4; i++)
+	{
+		k = i + j * k;
+	}
+	flag = 55;
+	if (flag == 10)
+		printf("Hello All");
+	getch();
+}
+
